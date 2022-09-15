4477c698170c31001faf89d322d52c24c3c97a38
Georgios Papadopoulos <linkin2895@gmail.com>
2022-09-15 11:51:55 +0100

    lambda simple calculator lesson

---
+++ src/Calculate.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculate {
+    public static void main(String[] args) {
+
+
+        Calculator calculator = new Calculator();
+        calculator.registerOperation("+", (a, b) -> a + b);
+        calculator.registerOperation("-", (a, b) -> a - b);
+        calculator.registerOperation("/", (a, b) -> a / b);
+        calculator.registerOperation("*", (a, b) -> a * b);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nPlease enter first operand: ");
+        int a = scanner.nextInt();
+        System.out.println("Please enter operator symbol: (+ - * or /)");
+        String operator = scanner.next();
+        System.out.println("Please enter second operand: ");
+        int b = scanner.nextInt();
+        scanner.close();
+        System.out.println("Result is: ");
+
+        System.out.println(calculator.calculate(a, operator, b));
+    }
+}
